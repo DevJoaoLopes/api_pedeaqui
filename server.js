3851e89ae97b7ab0708274e6a96372a7e39ea1fb
Otávio Barbosa <otavioromualdo@gmail.com>
2021-04-22 18:35:50 -0300

    Validar Token | Rotas sem token

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ const cors = require('cors')
 require("dotenv/config")
 const jwt = require('jsonwebtoken')
 const cookieParser = require('cookie-parser')
+const routesWithoutToken = require('./routesWithoutToken')
 
 /**
  * @description Arquivos
  */
-const teste = require('./app/routes/teste');
+const teste = require('./app/routes/teste')
 
 app.use(function(request, response, next) {
-    response.header('Access-Control-Allow-Origin', '*');
-    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    next();
-});
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    
+    validationToken(request, response, next)
+})
 
 app.use(cookieParser())
 app.use(bodyParser.json({limit: '50mb'}))
@@ -25,44 +26,36 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}))
 app.use(cors())
 
 
-app.set('port', process.env.API_PORT || 3131);
+app.set('port', process.env.API_PORT || 3131)
 
 const validationToken = (request, response, next) => {
     var token = request.headers['authorization']
-    if(!token){
-        response.statusCode = 500
-        return response.json({data: 'Token inválido ou inexistente'}) 
-    }
-    jwt.verify(token, process.env.SECRET, function(erro, decodificar) {
-        if(erro){
-            response.statusCode = 500
+
+    if(!routesWithoutToken.find((r) => { return r === request.originalUrl })){
+        if(!token){
+            response.statusCode = 401
             return response.json({data: 'Token inválido ou inexistente'}) 
-        } 
-        request.userId = decodificar.id
-        next()
-    });
+        }
+        jwt.verify(token, process.env.SECRET, (erro, decodificar) => {
+            if(erro){
+                response.statusCode = 401
+                return response.json({data: 'Token inválido ou inexistente'}) 
+            } 
+            request.user = decodificar
+        })
+    }
+    
+    next()
 }
 
 app.get(`/`, (request, response) => {
     return response.status(200).json({
-        data: 'Bem vindo a nossa API.'
+        data: 'Bem vindo a API da aplicação PedeAqui.'
     })
 })
 
 app.use(`/teste`, teste)
 
-if(parseInt(process.env.PRODUCAO) === 1){
-    app.listen(app.get('port'), () => {
-        console.log(`Port ${app.get('port')} foi inicializada`);
-    })
-}
-else{
-    const optionsSSL = {
-        key: fs.readFileSync(__dirname + process.env.KEYPEM),
-        cert: fs.readFileSync(__dirname + process.env.CERT),
-    }
-
-    https.createServer(optionsSSL, app).listen(app.get('port'), () => {
-        console.log(`Port ${app.get('port')} foi inicializada com SSL`);
-    })
-}
+app.listen(app.get('port'), () => {
+    console.log(`Port ${app.get('port')} foi inicializada`)
+})
