8bfa08d3667b1faebbbffd6d7490940aab87d2cf
Otávio Barbosa <otavioromualdo@gmail.com>
2021-09-28 21:29:29 -0300

    Rota de pedido | Rota de relação entre pedido e usuário | Rota de item pedido | Rota de relação entre item pedido e acompanhamento | Rota de relação entre item pedido e adicional

---
+++ app/routes/itens_pedidos_has_acompanhamentos.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const route = express.Router()
+const { mysql } = require("../helpers/mysql")
+const moment = require("moment")
+
+route.get('/', async (request, response) => {
+
+    let itens_pedidos_has_acompanhamentos = await mysql.queryAsync(`SELECT i.* FROM itens_pedidos_has_acompanhamentos AS i WHERE i.deleted_at IS NULL`)
+    
+    return response.status(200).json({
+        data: itens_pedidos_has_acompanhamentos
+    })
+
+})
+
+route.post('/', async (request, response) => {
+
+    const {item_pedido_id, acompanhamento_has_item_cardapio_id} = request.body
+
+    let registro = await mysql.queryAsync(`INSERT INTO itens_pedidos_has_acompanhamentos (item_pedido_id, acompanhamento_has_item_cardapio_id, created_at) VALUES (?, ?, ?)`, [item_pedido_id, acompanhamento_has_item_cardapio_id, moment().format('YYYY-MM-DD HH:mm:ss')])
+    
+    return response.status(200).json({
+        data: registro.insertId
+    })
+
+})
+
+route.put('/:id', async (request, response) => {
+
+    const {item_pedido_id, acompanhamento_has_item_cardapio_id} = request.body
+
+    await mysql.queryAsync(`UPDATE itens_pedidos_has_acompanhamentos SET item_pedido_id = ?, acompanhamento_has_item_cardapio_id = ?, updated_at = ? WHERE id = ?`, [item_pedido_id, acompanhamento_has_item_cardapio_id, moment().format('YYYY-MM-DD HH:mm:ss'), request.params.id])
+    
+    return response.status(200).json({
+        data: parseInt(request.params.id)
+    })
+
+})
+
+route.delete('/:id', async (request, response) => {
+
+    await mysql.queryAsync(`UPDATE itens_pedidos_has_acompanhamentos SET deleted_at = ? WHERE id = ?`, [moment().format('YYYY-MM-DD HH:mm:ss'), request.params.id])
+    
+    return response.status(200).json({
+        data: parseInt(request.params.id)
+    })
+})
+
+
+module.exports = route
---
+++ app/routes/pedidos.js
@@ -0,0 +1,50 @@
+const express = require("express")
+const route = express.Router()
+const { mysql } = require("../helpers/mysql")
+const moment = require("moment")
+
+route.get('/', async (request, response) => {
+
+    let pedidos = await mysql.queryAsync(`SELECT p.* FROM pedidos AS p`)
+    
+    return response.status(200).json({
+        data: pedidos
+    })
+
+})
+
+route.post('/', async (request, response) => {
+
+    const {mesa_id, estabelecimento_compartilhado_id} = request.body
+
+    let registro = await mysql.queryAsync(`INSERT INTO pedidos (mesa_id, estabelecimento_compartilhado_id, criado) VALUES (?, ?, ?)`, [mesa_id, estabelecimento_compartilhado_id, moment().format('YYYY-MM-DD HH:mm:ss')])
+    
+    return response.status(200).json({
+        data: registro.insertId
+    })
+
+})
+
+route.put('/:id', async (request, response) => {
+
+    const {mesa_id, estabelecimento_compartilhado_id, criado, encerrado} = request.body
+
+    await mysql.queryAsync(`UPDATE pedidos SET mesa_id = ?, estabelecimento_compartilhado_id = ?, criado = ?, encerrado = ? WHERE id = ?`, [mesa_id, estabelecimento_compartilhado_id, moment(criado).format('YYYY-MM-DD HH:mm:ss'), moment(encerrado).format('YYYY-MM-DD HH:mm:ss'), request.params.id])
+    
+    return response.status(200).json({
+        data: parseInt(request.params.id)
+    })
+
+})
+
+route.post('/encerrar/:id', async (request, response) => {
+
+    await mysql.queryAsync(`UPDATE pedidos SET encerrado = ? WHERE id = ?`, [moment().format('YYYY-MM-DD HH:mm:ss'), request.params.id])
+    
+    return response.status(200).json({
+        data: parseInt(request.params.id)
+    })
+
+})
+
+module.exports = route
