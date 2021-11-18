7a6945a9b062c400ea04a1ac0aaed9211eceb767
Otávio Barbosa <otavioromualdo@gmail.com>
2021-11-18 20:26:48 -0300

    Rota cadastrar-se

--- app/helpers/routes_without_token.js
+++ app/helpers/routes_without_token.js
@@ -2,6 +2,7 @@ module.exports = [
     { route: '/', method: 'GET' },
     { route: '/usuarios', method: 'POST' },
     { route: '/clientes', method: 'POST' },
+    { route: '/clientes/cadastrar_se', method: 'POST' },
     { route: '/estabelecimentos', method: 'POST' },
     { route: '/login', method: 'POST' },
     { route: '/imagens/*', method: 'GET' },
--- app/routes/clientes.js
+++ app/routes/clientes.js
@@ -16,12 +16,41 @@ const cpf_remover_texto = (cpf) => {
     return cpf.replace(/\D/g, '')
 } 
 
+const usuario_existe = async (usuario) => {
+    let user = await mysql.queryAsync(`SELECT u.* FROM usuarios AS u WHERE u.usuario = ?`, usuario)
+    return user
+} 
+
 route.get('/', async (request, response) => {
 
     let clientes = await mysql.queryAsync(`SELECT c.* FROM clientes AS c WHERE c.deleted_at IS NULL`)
     
     return response.status(200).json({
         data: clientes
+    })
+
+})
+
+route.post('/cadastrar_se', async (request, response) => {
+
+    let {nome, usuario, email, senha} = request.body
+
+    let validacao_usuario = await usuario_existe(usuario)
+
+    if(validacao_usuario.length > 0){
+        return response.status(400).json({
+            error: `Usuário já existe`
+        })
+    }
+
+    let cliente = await mysql.queryAsync(`INSERT INTO clientes (nome, created_at) VALUES (?, ?)`, [nome, moment().format('YYYY-MM-DD HH:mm:ss')])
+    let email_cliente = await mysql.queryAsync(`INSERT INTO emails (email, created_at) VALUES (?, ?)`, [email, moment().format('YYYY-MM-DD HH:mm:ss')])
+    let cliente_has_email = await mysql.queryAsync(`INSERT INTO clientes_has_emails (cliente_id, email_id) VALUES (?, ?)`, [cliente.insertId, email_cliente.insertId])
+
+    await mysql.queryAsync(`INSERT INTO usuarios (estabelecimento_id, cliente_id, estabelecimento_email_id, cliente_email_id, usuario, senha, created_at) VALUES (?, ?, ?, ?, ?, ?, ?)`, [null, cliente.insertId, null, cliente_has_email.insertId, usuario, senha, moment().format('YYYY-MM-DD HH:mm:ss')])
+    
+    return response.status(201).json({
+        data: cliente.insertId
     })
 
 })
