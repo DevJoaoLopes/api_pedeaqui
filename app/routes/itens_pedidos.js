3b793713b0f53f8f92e1b4cabffb5cf91d2f12a7
Otávio Barbosa <otavioromualdo@gmail.com>
2021-11-19 20:33:11 -0300

    Itens pedidos para a visualização do bar e do salão | Correção do cálculo relacionado ao valor total do item pedido

--- app/routes/itens_pedidos.js
+++ app/routes/itens_pedidos.js
@@ -16,13 +16,14 @@ route.get('/', async (request, response) => {
 route.get('/:pedido_id', async (request, response) => {
 
     let itens_pedidos = await mysql.queryAsync(`
-        SELECT i.*, sp.status, c.nome, ic.item, i.pedido_has_usuario_id FROM itens_pedidos AS i
+        SELECT i.*, sp.status, c.nome, ic.item, i.pedido_has_usuario_id, ic.valor FROM itens_pedidos AS i
         INNER JOIN status_pedidos AS sp ON sp.id = i.status_pedido_id
         INNER JOIN itens_cardapios AS ic ON ic.id = i.item_cardapio_id
         INNER JOIN pedidos_has_usuarios AS pu ON pu.id = i.pedido_has_usuario_id
         INNER JOIN usuarios AS u ON u.id = pu.usuario_id
         INNER JOIN clientes AS c ON c.id = u.cliente_id
         WHERE pu.pedido_id = ? AND i.deleted_at IS NULL
+        ORDER BY sp.id
     `, [request.params.pedido_id])
 
     let valor = 0
@@ -74,7 +75,7 @@ route.get('/:pedido_id', async (request, response) => {
         itens_pedidos.map((ip) => {
             let item_valor = 0
     
-            item_valor += ip.promocao.valor ? ip.promocao.valor : 0
+            item_valor += ip.promocao ? ip.promocao.valor : ip.valor
             item_valor += ip.acompanhamentos.filter(a => !a.obrigatorio).map(a => a.valor).reduce((x, y) => x + y, 0) 
             item_valor += ip.adicionais.map(a => a.valor).reduce((x, y) => x + y, 0) 
     
@@ -110,6 +111,136 @@ route.get('/estabelecimentos/cozinha/:estabelecimento_id', async (request, respo
         INNER JOIN usuarios AS u ON u.id = pu.usuario_id
         INNER JOIN clientes AS c ON c.id = u.cliente_id
         WHERE ic.estabelecimento_id = ? AND i.deleted_at IS NULL AND s.setor = 'Cozinha'
+    `, [request.params.estabelecimento_id])
+    //     WHERE ic.estabelecimento_id = ? AND i.deleted_at IS NULL AND
+    //     i.created_at >= ? AND i.created_at <= ?
+    // `, [request.params.estabelecimento_id, inicio, termino])
+
+    if(itens_pedidos.length > 0){
+        let promocoes = await mysql.queryAsync(`
+            SELECT p.* FROM promocoes AS p
+            WHERE p.id IN (?)
+        `, [itens_pedidos.map(i => i.promocao_id ? i.promocao_id : 0)])
+        
+        let acompanhamentos = await mysql.queryAsync(`
+            SELECT ia.*, a.acompanhamento, a.obrigatorio FROM itens_pedidos_has_acompanhamentos AS ia
+            INNER JOIN acompanhamentos_has_itens_cardapios AS ai ON ai.id = ia.acompanhamento_has_item_cardapio_id
+            INNER JOIN acompanhamentos AS a ON a.id = ai.acompanhamento_id
+            WHERE ia.item_pedido_id IN (?) AND ia.deleted_at IS NULL
+        `, [itens_pedidos.map(i => i.id)])
+        
+        let adicionais = await mysql.queryAsync(`
+            SELECT ia.*, a.adicional FROM itens_pedidos_has_adicionais AS ia
+            INNER JOIN adicionais_has_itens_cardapios AS ai ON ai.id = ia.adicional_has_item_cardapio_id
+            INNER JOIN adicionais_itens AS a ON a.id = ai.adicional_id
+            WHERE ia.item_pedido_id IN (?) AND ia.deleted_at IS NULL
+        `, [itens_pedidos.map(i => i.id)])
+
+        let escolhas = await mysql.queryAsync(`
+            SELECT ie.*, e.escolha, eo.opcao FROM itens_pedidos_has_escolhas AS ie
+            INNER JOIN escolhas_has_itens_cardapios AS ei ON ei.id = ie.escolha_has_item_cardapio_id
+            INNER JOIN escolhas AS e ON e.id = ei.escolha_id
+            INNER JOIN escolhas_opcoes AS eo ON eo.id = ie.escolha_opcao_id
+            WHERE ie.item_pedido_id IN (?) AND ie.deleted_at IS NULL
+        `, [itens_pedidos.map(i => i.id)])
+        
+        itens_pedidos.map((i) => {
+            i.promocao = promocoes.find(p => p.id === i.promocao_id)
+            i.acompanhamentos = acompanhamentos.filter(a => a.item_pedido_id === i.id)
+            i.adicionais = adicionais.filter(a => a.item_pedido_id === i.id)
+            i.escolhas = escolhas.filter(e => e.item_pedido_id === i.id)
+    
+            return null
+        })
+    }
+
+    return response.status(200).json({
+        data: itens_pedidos
+    })
+
+})
+
+route.get('/estabelecimentos/bar/:estabelecimento_id', async (request, response) => {
+
+    let inicio = moment().subtract(5, 'hours').format('YYYY-MM-DD HH:mm:ss')
+    let termino = moment().format('YYYY-MM-DD HH:mm:ss')
+
+    let itens_pedidos = await mysql.queryAsync(`
+        SELECT i.*, sp.status, c.nome, ic.item, i.pedido_has_usuario_id, s.setor AS preparado_por, m.identificacao FROM itens_pedidos AS i
+        INNER JOIN status_pedidos AS sp ON sp.id = i.status_pedido_id
+        INNER JOIN itens_cardapios AS ic ON ic.id = i.item_cardapio_id
+        INNER JOIN setores AS s ON s.id = ic.preparado_por_id
+        INNER JOIN pedidos_has_usuarios AS pu ON pu.id = i.pedido_has_usuario_id
+        INNER JOIN pedidos AS p ON p.id = pu.pedido_id
+        INNER JOIN mesas AS m ON m.id = p.mesa_id
+        INNER JOIN usuarios AS u ON u.id = pu.usuario_id
+        INNER JOIN clientes AS c ON c.id = u.cliente_id
+        WHERE ic.estabelecimento_id = ? AND i.deleted_at IS NULL AND s.setor = 'Bar'
+    `, [request.params.estabelecimento_id])
+    //     WHERE ic.estabelecimento_id = ? AND i.deleted_at IS NULL AND
+    //     i.created_at >= ? AND i.created_at <= ?
+    // `, [request.params.estabelecimento_id, inicio, termino])
+
+    if(itens_pedidos.length > 0){
+        let promocoes = await mysql.queryAsync(`
+            SELECT p.* FROM promocoes AS p
+            WHERE p.id IN (?)
+        `, [itens_pedidos.map(i => i.promocao_id ? i.promocao_id : 0)])
+        
+        let acompanhamentos = await mysql.queryAsync(`
+            SELECT ia.*, a.acompanhamento, a.obrigatorio FROM itens_pedidos_has_acompanhamentos AS ia
+            INNER JOIN acompanhamentos_has_itens_cardapios AS ai ON ai.id = ia.acompanhamento_has_item_cardapio_id
+            INNER JOIN acompanhamentos AS a ON a.id = ai.acompanhamento_id
+            WHERE ia.item_pedido_id IN (?) AND ia.deleted_at IS NULL
+        `, [itens_pedidos.map(i => i.id)])
+        
+        let adicionais = await mysql.queryAsync(`
+            SELECT ia.*, a.adicional FROM itens_pedidos_has_adicionais AS ia
+            INNER JOIN adicionais_has_itens_cardapios AS ai ON ai.id = ia.adicional_has_item_cardapio_id
+            INNER JOIN adicionais_itens AS a ON a.id = ai.adicional_id
+            WHERE ia.item_pedido_id IN (?) AND ia.deleted_at IS NULL
+        `, [itens_pedidos.map(i => i.id)])
+
+        let escolhas = await mysql.queryAsync(`
+            SELECT ie.*, e.escolha, eo.opcao FROM itens_pedidos_has_escolhas AS ie
+            INNER JOIN escolhas_has_itens_cardapios AS ei ON ei.id = ie.escolha_has_item_cardapio_id
+            INNER JOIN escolhas AS e ON e.id = ei.escolha_id
+            INNER JOIN escolhas_opcoes AS eo ON eo.id = ie.escolha_opcao_id
+            WHERE ie.item_pedido_id IN (?) AND ie.deleted_at IS NULL
+        `, [itens_pedidos.map(i => i.id)])
+        
+        itens_pedidos.map((i) => {
+            i.promocao = promocoes.find(p => p.id === i.promocao_id)
+            i.acompanhamentos = acompanhamentos.filter(a => a.item_pedido_id === i.id)
+            i.adicionais = adicionais.filter(a => a.item_pedido_id === i.id)
+            i.escolhas = escolhas.filter(e => e.item_pedido_id === i.id)
+    
+            return null
+        })
+    }
+
+    return response.status(200).json({
+        data: itens_pedidos
+    })
+
+})
+
+route.get('/estabelecimentos/salao/:estabelecimento_id', async (request, response) => {
+
+    let inicio = moment().subtract(5, 'hours').format('YYYY-MM-DD HH:mm:ss')
+    let termino = moment().format('YYYY-MM-DD HH:mm:ss')
+
+    let itens_pedidos = await mysql.queryAsync(`
+        SELECT i.*, sp.status, c.nome, ic.item, i.pedido_has_usuario_id, s.setor AS preparado_por, m.identificacao FROM itens_pedidos AS i
+        INNER JOIN status_pedidos AS sp ON sp.id = i.status_pedido_id
+        INNER JOIN itens_cardapios AS ic ON ic.id = i.item_cardapio_id
+        INNER JOIN setores AS s ON s.id = ic.preparado_por_id
+        INNER JOIN pedidos_has_usuarios AS pu ON pu.id = i.pedido_has_usuario_id
+        INNER JOIN pedidos AS p ON p.id = pu.pedido_id
+        INNER JOIN mesas AS m ON m.id = p.mesa_id
+        INNER JOIN usuarios AS u ON u.id = pu.usuario_id
+        INNER JOIN clientes AS c ON c.id = u.cliente_id
+        WHERE ic.estabelecimento_id = ? AND i.deleted_at IS NULL
     `, [request.params.estabelecimento_id])
     //     WHERE ic.estabelecimento_id = ? AND i.deleted_at IS NULL AND
     //     i.created_at >= ? AND i.created_at <= ?
